fa6f95e304785c3753aeabff4a7f02a4137e420d
Henry <henry@kirkness.com>
2019-01-20 12:13:23 +0000

    Eslint + Prettier partially working

--- packages/api/lib/api.ts
+++ packages/api/lib/api.ts
@@ -1,28 +1,32 @@
-require('dotenv').config();
-import { ApolloServer } from 'apollo-server';
-import gql from 'graphql-tag';
-import { knex } from '@monad.club/databases';
-import { withEmoji } from '@monad.club/utils';
+import { ApolloServer } from "apollo-server";
+import gql from "graphql-tag";
+import { knex } from "@monad.club/databases";
+import { withEmoji } from "@monad.club/utils";
+
+require("dotenv").config();
 
 const server = new ApolloServer({
-	typeDefs: gql`
-		type Query {
-			testDbConnection: Int
-			hello: String
-		}
-	`,
-	resolvers: {
-		Query: {
-			testDbConnection: async () => {
-				const { rows: [ row ] } = await knex.raw(`SELECT 1 as one`);
-				return row.one;
-			},
-			hello: () => withEmoji('heya there')
-		}
-	}
+  typeDefs: gql`
+    type Query {
+      testDbConnection: Int
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      testDbConnection: async () => {
+        const {
+          rows: [row]
+        } = await knex.raw(`SELECT 1 as one`);
+        return row.one;
+      },
+      hello: () => withEmoji("heya there")
+    }
+  }
 });
 
 const port = process.env.PORT || 4000;
 server.listen(port).then(({ url }) => {
-	console.log(`👋  GraphQL server started at ${url}`);
+  // eslint-disable-next-line no-console
+  console.log(`👋  GraphQL server started at ${url}`);
 });
